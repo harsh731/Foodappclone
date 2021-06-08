0abd6cc5cb34df25d542b8780b469d196ea21d44
Harsh <kushwahah330@gmail.com>
2021-06-08 20:37:06 +0530

    Last Update

---
+++ final_app_/frontend/src/component/details/DetailsComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+const Details = () => {
+    return(
+        <div className="container">
+            <div className="panel panel-success">
+                <div className="panel-heading">
+                    Hotel Detail
+                </div>
+                <div className="panel-body">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    <p>
+                    Lorem Ipsum छपाई और अक्षर योजन उद्योग का एक साधारण डमी पाठ है. Lorem Ipsum सन १५०० के बाद से अभी तक इस उद्योग का मानक डमी पाठ मन गया, जब एक अज्ञात मुद्रक ने नमूना लेकर एक नमूना किताब बनाई. यह न केवल पाँच सदियों से जीवित रहा बल्कि इसने इलेक्ट्रॉनिक मीडिया में छलांग लगाने के बाद भी मूलतः अपरिवर्तित रहा. यह 1960 के दशक में Letraset Lorem Ipsum अंश युक्त पत्र के रिलीज के साथ लोकप्रिय हुआ, और हाल ही में Aldus PageMaker Lorem Ipsum के संस्करणों सहित तरह डेस्कटॉप प्रकाशन सॉफ्टवेयर के साथ अधिक प्रचलित हुआ.
+                    </p>
+                    <a href="https://www.facebook.com" target="_blank">FaceBook</a>
+                    <div className="row">
+                        <Tabs>
+                            <TabList>
+                                <Tab>Address</Tab>
+                                <Tab>Contact</Tab>
+                            </TabList>
+                            <TabPanel>
+                                <h2>Any content 1</h2>
+                            </TabPanel>
+                            <TabPanel>
+                                <h2>Any content 2</h2>
+                            </TabPanel>
+                        </Tabs>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Details;
